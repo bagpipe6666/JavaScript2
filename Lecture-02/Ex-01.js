411994250bddc0eac91549b619dc0a894e28b29b
bagpipe6666 <bumpoloyou@gmail.com>
2023-12-06 11:31:42 +0700

    up

---
+++ Lecture-02/Ex-01.js
@@ -0,0 +1,12 @@
+let num1 = 6;
+let num2 = 7;
+let num3 = 20;
+let num4 = 10;
+let num5 = 2;
+let num6 = 9;
+
+// console.log(parseInt(num1 + num2)*2);
+// console.log(parseInt(num3 + num4)*2);
+// console.log(parseInt(num5 + num6)*2);
+
+
